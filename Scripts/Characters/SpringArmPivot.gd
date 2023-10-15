44b56106d405eac6db1fc845736e1bd38d74c877
WaffleAWT <alinajjarwork@gmail.com>
2023-10-15 13:38:05 +0300

    Added FOV exports

--- Scripts/Characters/SpringArmPivot.gd
+++ Scripts/Characters/SpringArmPivot.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
-@export var NORMAL_FOV : float = 75.0
-@export var RUN_FOV : float = 90.0
+@export_group("FOV")
+@export var change_fov_on_run : bool
+@export var normal_fov : float = 75.0
+@export var run_fov : float = 90.0
 
 const CAMERA_BLEND : float = 0.05
 
@@ -18,10 +20,11 @@ func _unhandled_input(event):
 		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
 
 func _physics_process(_delta):
-	if owner.is_on_floor():
-		if Input.is_action_pressed("run"):
-			camera.fov = lerp(camera.fov, RUN_FOV, CAMERA_BLEND)
+	if change_fov_on_run:
+		if owner.is_on_floor():
+			if Input.is_action_pressed("run"):
+				camera.fov = lerp(camera.fov, run_fov, CAMERA_BLEND)
+			else:
+				camera.fov = lerp(camera.fov, normal_fov, CAMERA_BLEND)
 		else:
-			camera.fov = lerp(camera.fov, NORMAL_FOV, CAMERA_BLEND)
-	else:
-		camera.fov = lerp(camera.fov, NORMAL_FOV, CAMERA_BLEND)
+			camera.fov = lerp(camera.fov, normal_fov, CAMERA_BLEND)
